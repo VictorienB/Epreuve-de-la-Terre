05db400478945661007aff74317dbb5cb6f08f6c
VictorienB <victorien63970@gmail.com>
2023-09-14 15:33:12 +0200

    terre03 fini

--- terre03.js
+++ terre03.js
@@ -4,32 +4,27 @@
 // Récupérer les arguments après le nom du fichier
 const n = process.argv.slice(2);
 
+if (n.length === 0) {
+  console.log("Aucun argument fourni.");
+} else {
+  const letter = n[0];
 
-// Je vérifie que c'est juste un nombre/chiffre
-function estComposeeDeChiffres(chaine) {
-    // Utilisez une expression régulière pour vérifier si la chaîne ne contient que des chiffres.
-    return /^[0-9]+$/.test(chaine);
+  // Vérifiez que la chaîne est une lettre en minuscules
+  if (/^[a-z]$/.test(letter)) {
+    const codeLettre = letter.charCodeAt(0);
+
+    // Je créé l'alphabet
+    let alphabet = '';
+
+    // Je le remplis
+    for (let i = codeLettre; i <= 122; i++) {
+      const lettre = String.fromCharCode(i);
+      alphabet += lettre;
+    }
+
+    // Je le renvoie avec un retour à la ligne
+    console.log(alphabet);
+  } else {
+    console.log("L'argument n'est pas une lettre en minuscules.");
+  }
 }
-
-resultat=estComposeeDeChiffres(n);
-console.log(resultat);
-
-if ( resultat=false ) {
-    console.log("erreur ce n'est pas un chiffre ou un nombre")
-}
-  
-// Je passe n en int
-const nEntier = +n;
-// Je renvoi l'alpohabet en fonction en reprenant le code déjà fait précédemment
-
-// je cré l'alphabet
-let alphabet = '';
-
-
-//je la rempli
-for (let i = 97+nEntier; i <= 122; i++) {
-  const letter = String.fromCharCode(i);
-  alphabet += letter;
-}
-// Je la renvoi avec une fonction qui met un retour a la ligne ensuite
-console.log(alphabet);
