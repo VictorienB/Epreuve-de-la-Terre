b38127f401b15db49dae8f9b8e3301ec164b4d18
VictorienB <victorien63970@gmail.com>
2023-09-14 13:56:06 +0200

    Commentaire terre00 et creation fichier terre01

--- terre00.js
+++ terre00.js
@@ -1,8 +1,12 @@
+
+// je cré l'alphabet
 let alphabet = '';
 
+
+//je la rempli
 for (let i = 97; i <= 122; i++) {
   const letter = String.fromCharCode(i);
   alphabet += letter;
 }
-
+// Je la renvoi avec une fonction qui met un retour a la ligne ensuite
 console.log(alphabet);
