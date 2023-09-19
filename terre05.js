c50c40471245fceed1ef1995ea717d3fad027309
VictorienB <victorien63970@gmail.com>
2023-09-19 12:16:27 +0200

    fin epreuve 5 :)

--- terre05.js
+++ terre05.js
@@ -2,17 +2,17 @@
 
 const args = process.argv.slice(2);
 
-if (args.length !== 2) {
-  console.log("Usage: node exo.js <nombre1> <nombre2>");
+if (args.length !== 2 || args[1] == 0 || parseInt(args[0]) < parseInt(args[1])) {
+  console.log("erreur");
 } else {
   const nombre1 = parseInt(args[0]);
   const nombre2 = parseInt(args[1]);
-
+  
   if (!isNaN(nombre1) && !isNaN(nombre2)) {
     const resultat = nombre1 / nombre2;
     const reste = nombre1 % nombre2;
-
-    console.log(`résultat: ${resultat}`);
+    const resultatE = Math.floor(resultat); // je prend la partie entiere du résultat
+    console.log(`résultat: ${resultatE}`);
     console.log(`reste: ${reste}`);
   } else {
     console.log("erreur.");
