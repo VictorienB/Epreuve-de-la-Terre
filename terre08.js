01013b9521a67eb7b249e46648d7d47e92f71efe
VictorienB <victorien63970@gmail.com>
2023-09-19 14:07:18 +0200

    terre08 fini

---
+++ terre08.js
@@ -0,0 +1,20 @@
+// créer un programme qui affiche le résultat d'une puissance
+
+// Je récupere les arguments
+const args = process.argv.slice(2);
+// je vérifie que ya bien que 2 arguments
+if (args[0] == undefined || args[2] != undefined || args[1] == undefined || isNaN(args[0]) || isNaN(args[1])) {
+    console.log("erreur: mettez seulement d'");
+ } else if ( args[1] == 0 ) {
+    console.log("Le résultat est :" + 1)
+ } else if ( args[0] == 0 ) {
+    console.log("Le résultat est :" + 0)
+ } else {
+//Je fais une boucle pour faire la puissance des deux
+// Je met l'exception si puissance 0 =1
+        resultat = args[0];
+        for (let i = 2; i<=args[1]; i++) {
+            resultat*=args[0];
+        }
+ }
+console.log(resultat);
